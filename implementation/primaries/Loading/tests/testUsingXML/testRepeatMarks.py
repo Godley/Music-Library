03c6339f9ab9d354e29e217cf305d762d7ce9b89
Godley <charlotte.godley@hotmail.co.uk>
2014-12-25 18:08:29 +0000

    Repeat mark tests initial commit

---
+++ implementation/primaries/Loading/tests/testUsingXML/testRepeatMarks.py
@@ -0,0 +1,22 @@
+from implementation.primaries.Loading.tests.testUsingXML.setup import xmlSet, parsePiece
+from implementation.primaries.Loading.classes import Note
+import os
+
+partname = "repeatmarks.xml"
+folder = "/Users/charlottegodley/PycharmProjects/FYP/implementation/primaries/SampleMusicXML/testcases"
+piece = parsePiece(os.path.join(folder, partname))
+#TODO: handle forward tags
+class testFile(xmlSet):
+    def setUp(self):
+        xmlSet.setUp(self)
+        self.m_num = 32
+        self.p_id = "P1"
+        self.p_name = "Flute"
+
+    def testParts(self):
+        global piece
+        self.assertTrue(self.p_id in piece.Parts)
+        self.assertEqual(self.p_name, piece.Parts[self.p_id].name)
+
+    def testMeasures(self):
+        self.assertTrue(self.m_num in piece.Parts[self.p_id].measures)
