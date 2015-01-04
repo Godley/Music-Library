9bb46a70534d0df472fc1dd5a3ad8eb2dc1da2fd
Godley <charlotte.godley@hotmail.co.uk>
2015-01-04 01:37:32 +0000

    tests and code for meter lily method

--- implementation/primaries/Drawing/classes/Meter.py
+++ implementation/primaries/Drawing/classes/Meter.py
@@ -7,3 +7,21 @@ class Meter(object):
 
     def __str__(self):
         return str(self.beats) + "/" + str(self.type)
+
+    def toLily(self):
+        val = "\time"
+        if hasattr(self, "beats"):
+            val += " " + str(self.beats)
+            if hasattr(self, "type"):
+                val += "/" + str(self.type)
+            else:
+                if self.beats <= 4:
+                    val += "/4"
+                elif 4 < self.beats <= 8:
+                    val += "/8"
+        elif not hasattr(self, "type"):
+            val += " 4/4"
+        else:
+            val += " " + str(self.type) + "/" + str(self.type)
+
+        return val
