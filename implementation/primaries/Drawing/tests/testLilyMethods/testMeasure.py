85e7c14145387f6d77935b65da17d714dc18d3e9
Godley <charlotte.godley@hotmail.co.uk>
2015-01-17 00:50:08 +0000

    added initial tests for measure class

--- implementation/primaries/Drawing/tests/testLilyMethods/testMeasure.py
+++ implementation/primaries/Drawing/tests/testLilyMethods/testMeasure.py
@@ -1,4 +1,4 @@
-from implementation.primaries.Drawing.classes import Measure
+from implementation.primaries.Drawing.classes import Measure, Note
 from implementation.primaries.Drawing.tests.testLilyMethods.setup import Lily
 
 class testMeasure(Lily):
@@ -11,4 +11,11 @@ class testMeasureStaves(Lily):
         self.item = Measure.Measure()
         self.item.items[2] = []
         self.lilystring = "\\new PianoStaff <<\n \\new Staff {} \n \\new Staff {} \n>>"
+
+class testMeasureNote(Lily):
+    def setUp(self):
+        self.item = Measure.Measure()
+        self.item.items[1].append(Note.Note())
+        self.item.items[1][0].pitch = Note.Pitch()
+        self.lilystring = "\\new Staff {c'}"
 
