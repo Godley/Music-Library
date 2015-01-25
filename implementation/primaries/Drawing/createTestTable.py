bf74ffe6c5eb2aa04dffdd8eae2d6de9953f3487
Godley <charlotte.godley@hotmail.co.uk>
2015-01-25 18:17:23 +0000

    improved on test table generator - now outputs to CSV

--- implementation/primaries/Drawing/createTestTable.py
+++ implementation/primaries/Drawing/createTestTable.py
@@ -17,7 +17,6 @@ def statement_type(line):
     if "assert" in line:
         return "assert"
     if "=" in line:
-        print(line)
         return "assignment"
 
 def ParseInstance(line):
@@ -43,7 +42,6 @@ def ParseBool(line):
             word += line[index]
             if len(word) > 2:
                 if  word[-2] + word[-1] == "in":
-                    print(word)
                     break
         word = word[:-2]
         expected += "the value " + word + "is in the iterable or string " + line[len(word)+2:]
@@ -60,6 +58,9 @@ def ParseEqual(line):
     parts = line.split(",")
     expected = ""
     variables = None
+    if "\"" not in parts[0]:
+        if parts[0] in assignments.keys():
+            parts[0] = assignments[parts[0]]
     if isMethod(parts[1]):
         if "len" in parts[1]:
             thing = removeParamBrackets(parts[1][4:])
@@ -68,14 +69,13 @@ def ParseEqual(line):
             expected += parts[1] + " has a return value of " + parts[0]
     else:
         expected += parts[1] + " is equal to " + parts[0]
-        if "\"" not in parts[0]:
-            variables = parts[0]
+
     return expected, variables
 
 for root, dirs, files in os.walk(folder, topdown=False):
     [Add(f, root.split("/")[-1]) for f in files if f.endswith("py") and f != "__init__.py"]
 variables_second_pass = []
-assignments = []
+assignments = {}
 for pyfile in file_list.keys():
     if file_list[pyfile] not in csv_dict:
         csv_dict[file_list[pyfile]] = {}
@@ -88,7 +88,9 @@ for pyfile in file_list.keys():
         class_regex = "class test"
         func_regex = "def test"
         assert_reg = "self.assert"
-
+        if "=" in line:
+            var_and_value = line.split("=")
+            assignments[var_and_value[0]] = var_and_value[1]
 
         if class_regex in line:
             list_of_string = list(line)
@@ -123,25 +125,30 @@ for pyfile in file_list.keys():
             comparison = line_without_assert[len(test)+1:-2]
             value = "".join(comparison)
             expected, variables = options[test](value)
-            variables_second_pass.append(variables)
             csv_dict[file_list[pyfile]][pyfile][classname][testname] = [test,expected]
     fob.close()
-    fob = open(os.path.join(folder,file_list[pyfile], pyfile), 'r')
-    for line in fob.readline():
-        print(line)
-        if statement_type(line) == "assignment":
-            for variable in variables_second_pass:
-                if variable in line:
-                    assignments.append(line)
 
-for line in assignments:
-    print(line)
+fname = "/Users/charlottegodley/PycharmProjects/FYP/implementation/primaries/mytests.csv"
+
+csv_list = []
+
 for folder in csv_dict:
-    print("Folder: ", folder)
+    #print("Folder: ", folder)
     for file in csv_dict[folder]:
-        print("File: ", file)
+        #print("File: ", file)
         for classname in csv_dict[folder][file]:
-            print("classname: ", classname)
+            #print("classname: ", classname)
             for function in csv_dict[folder][file][classname]:
-                print("function: ", function)
-                print("assert: ", csv_dict[folder][file][classname][function][0], "expected result: ", csv_dict[folder][file][classname][function][1])
+                csv_list.append({"folder":folder, "file":file, "class": classname, "function": function})
+                if csv_dict[folder][file][classname][function] != "":
+                    csv_list[-1]["test type"] = csv_dict[folder][file][classname][function][0]
+                    csv_list[-1]["expected result"] = csv_dict[folder][file][classname][function][1]
+                #print("function: ", function)
+                #print("assert: ", csv_dict[folder][file][classname][function][0], "expected result: ", csv_dict[folder][file][classname][function][1])
+
+fields = ["folder","file","class","function","test type", "expected result"]
+with open(fname, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fields)
+    writer.writeheader()
+    for entry in csv_list:
+        writer.writerow(entry)
