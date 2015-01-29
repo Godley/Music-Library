e60e502398191a946b558687f85d566903c99f93
Godley <charlotte.godley@hotmail.co.uk>
2015-01-29 00:18:48 +0000

    updated simple script so it now works and does output of pdf file to appropriate folder, needs testing on other testcases

--- implementation/primaries/Drawing/run.py
+++ implementation/primaries/Drawing/run.py
@@ -1,10 +1,13 @@
+import os
 from implementation.primaries.Drawing.classes import Piece, MxmlParser
 import os
 
+testcases = '/Users/charlottegodley/PycharmProjects/FYP/implementation/primaries/SampleMusicXML/testcases'
 file = '/Users/charlottegodley/PycharmProjects/FYP/implementation/primaries/SampleMusicXML/testcases/accidentals.xml'
 parser = MxmlParser.MxmlParser()
 pieceObj = parser.parse(file)
 lilypond_file = file.split(".")[0] + ".ly"
 fob = open(lilypond_file, 'w')
 fob.write(pieceObj.toLily())
-os.system("lilypond "+lilypond_file)
+fob.close()
+os.system("/Users/charlottegodley/bin/lilypond --output "+testcases+" "+lilypond_file)
