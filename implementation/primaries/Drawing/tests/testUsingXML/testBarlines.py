415622c109eb2f0081fcc5f5e3d749c13fdd3531
Godley <charlotte.godley@hotmail.co.uk>
2015-02-15 20:58:36 +0000

    general test fixes

--- implementation/primaries/Drawing/tests/testUsingXML/testBarlines.py
+++ implementation/primaries/Drawing/tests/testUsingXML/testBarlines.py
@@ -11,7 +11,7 @@ class testBarlines(xmlSet):
         xmlSet.setUp(self)
         self.m_num = 32
         self.p_id = "P1"
-        self.p_name = "Flute"
+        self.p_name = "Piano"
 
     def testParts(self):
         global piece
@@ -35,7 +35,7 @@ class testBarlines(xmlSet):
 
     def testMeasure2BarlineStyle(self):
         item = piece.Parts[self.p_id].measures[1][2]
-        self.assertEqual("dotted", item.barlines["right"].style)
+        self.assertEqual("dashed", item.barlines["right"].style)
 
     def testMeasure4Barline(self):
         item = piece.Parts[self.p_id].measures[1][4]
@@ -43,40 +43,15 @@ class testBarlines(xmlSet):
 
     def testMeasure4BarlineInstance(self):
         item = piece.Parts[self.p_id].measures[1][4]
-        self.assertIsInstance(item.barlines["left"], Measure.Barline)
         self.assertIsInstance(item.barlines["right"], Measure.Barline)
-
-    def testMeasure4BarlineLeft(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertTrue("left" in item.barlines)
 
     def testMeasure4BarlineRight(self):
         item = piece.Parts[self.p_id].measures[1][4]
         self.assertTrue("right" in item.barlines)
 
-    def testMeasure4BarlineLeftStyle(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertEqual("heavy-light", item.barlines["left"].style)
-
     def testMeasure4BarlineRightStyle(self):
         item = piece.Parts[self.p_id].measures[1][4]
         self.assertEqual("light-heavy", item.barlines["right"].style)
-
-    def testMeasure4BarlineLeftRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertTrue(hasattr(item.barlines["left"], "repeat"))
-
-    def testMeasure4BarlineLeftRepeatVal(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertEqual("forward", item.barlines["left"].repeat)
-
-    def testMeasure4BarlineRightRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertTrue(hasattr(item.barlines["right"], "repeat"))
-
-    def testMeasure4BarlineRightRepeatVal(self):
-        item = piece.Parts[self.p_id].measures[1][4]
-        self.assertEqual("backward", item.barlines["right"].repeat)
 
     def testMeasure5Barline(self):
         item = piece.Parts[self.p_id].measures[1][5]
@@ -93,111 +68,3 @@ class testBarlines(xmlSet):
     def testMeasure5BarlineStyle(self):
         item = piece.Parts[self.p_id].measures[1][5]
         self.assertEqual("light-light", item.barlines["right"].style)
-
-    def testMeasure6BarlineRight(self):
-        item = piece.Parts[self.p_id].measures[1][6]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure6BarlineRight(self):
-        item = piece.Parts[self.p_id].measures[1][6]
-        self.assertTrue("right" in item.barlines)
-
-    def testMeasure6BarlineRightInstance(self):
-        item = piece.Parts[self.p_id].measures[1][6]
-        self.assertIsInstance(item.barlines["right"], Measure.Barline)
-
-    def testMeasure6BarlineStyle(self):
-        item = piece.Parts[self.p_id].measures[1][6]
-        self.assertEqual("light-heavy", item.barlines["right"].style)
-
-    def testMeasure7BarlineLeft(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure7BarlineLeft(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertTrue("left" in item.barlines)
-
-    def testMeasure7BarlineLeftInstance(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertIsInstance(item.barlines["left"], Measure.Barline)
-
-    def testMeasure7BarlineStyle(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertEqual("heavy-light", item.barlines["left"].style)
-
-    def testMeasure7BarlineLeftRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertEqual("forward", item.barlines["left"].repeat)
-
-    def testMeasure7BarlineCreated(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure7BarlineRight(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertTrue("right" in item.barlines)
-
-    def testMeasure7BarlineRightInstance(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertIsInstance(item.barlines["right"], Measure.Barline)
-
-    def testMeasure7BarlineRightStyle(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertEqual("light-heavy", item.barlines["right"].style)
-
-    def testMeasure7BarlineRightRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][7]
-        self.assertEqual("backward", item.barlines["right"].repeat)
-
-    def testMeasure8BarlineLeft(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure8BarlineLeftVal(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertTrue("left" in item.barlines)
-
-    def testMeasure8BarlineLeftInstance(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertIsInstance(item.barlines["left"], Measure.Barline)
-
-    def testMeasure8BarlineStyle(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertEqual("heavy-light", item.barlines["left"].style)
-
-    def testMeasure8BarlineLeftRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertEqual("forward", item.barlines["left"].repeat)
-
-    def testMeasure8BarlineCreated(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure8BarlineRight(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertTrue("right" in item.barlines)
-
-    def testMeasure8BarlineRightInstance(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertIsInstance(item.barlines["right"], Measure.Barline)
-
-    def testMeasure8BarlineRightStyle(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertEqual("light-heavy", item.barlines["right"].style)
-
-    def testMeasure8BarlineRightRepeat(self):
-        item = piece.Parts[self.p_id].measures[1][8]
-        self.assertEqual("backward", item.barlines["right"].repeat)
-
-    def testMeasure9Barline(self):
-        item = piece.Parts[self.p_id].measures[1][9]
-        self.assertTrue(hasattr(item, "barlines"))
-
-    def testMeasure9BarlineRight(self):
-        item = piece.Parts[self.p_id].measures[1][9]
-        self.assertTrue("right" in item.barlines)
-
-    def testMeasure9BarlineStyle(self):
-        item = piece.Parts[self.p_id].measures[1][9]
-        self.assertEqual("light-heavy", item.barlines["right"].style)
