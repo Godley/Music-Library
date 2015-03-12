d1aa89d103d0e81933b428fe927868eae6ba1d75
Godley <charlotte.godley@hotmail.co.uk>
2015-03-12 20:59:10 +0000

    added a second testcase
    - added testcase2.xml to test beat=beat metronome markings
    - added unit tests and confirmed they pass
    - issue fix #186

--- implementation/primaries/ExtractMetadata/tests/testMetaParserUsingXML.py
+++ implementation/primaries/ExtractMetadata/tests/testMetaParserUsingXML.py
@@ -27,3 +27,30 @@ class testCase1(unittest.TestCase):
 
     def testComposer(self):
         self.assertEqual(self.result["composer"], "Charlotte Godley")
+
+class testCase2(unittest.TestCase):
+    def setUp(self):
+        self.file = '/Users/charlottegodley/PycharmProjects/FYP/implementation/primaries/ExtractMetadata/tests/test_files/extractor_test/testcase2.xml'
+        self.parser = MetaParser.MetaParser()
+        self.result = self.parser.parse(self.file)
+
+    def testInstruments(self):
+        self.assertEqual(self.result["instruments"], [{"name":"Piano"}])
+
+    def testClefs(self):
+        self.assertEqual(self.result["clef"], {"Piano":[{"sign":"G","line":2},{"sign":"F","line":4},{"line":3,"sign":"C"}]})
+
+    def testKeys(self):
+        self.assertEqual(self.result["key"], {"Piano":[{"fifths":2,"mode":"major"}]})
+
+    def testTempos(self):
+        self.assertEqual(self.result["tempo"], [{"beat":"half","beat_2":"quarter"},{"minute":80,"beat":"eighth."}])
+
+    def testTitle(self):
+        self.assertEqual(self.result["title"], "My MetaParsing Testcase")
+
+    def testLyricist(self):
+        self.assertEqual(self.result["lyricist"], "Fran Godley")
+
+    def testComposer(self):
+        self.assertEqual(self.result["composer"], "Charlotte Godley")
