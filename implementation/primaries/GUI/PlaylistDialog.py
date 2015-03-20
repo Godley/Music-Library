3bda5ef9261717f17b12c6120698a488206143dd
Godley <charlotte.godley@hotmail.co.uk>
2015-03-20 00:45:51 +0000

    added method for autocomplete to new playlist box

--- implementation/primaries/GUI/PlaylistDialog.py
+++ implementation/primaries/GUI/PlaylistDialog.py
@@ -7,7 +7,10 @@ class PlaylistDialog(QtGui.QDialog):
         self.parent = parent
         QtGui.QDialog.__init__(self)
         uic.loadUi('NewPlaylist.ui', self)
+        self.autoCompleteFrame.hide()
         self.buttonBox.accepted.connect(self.newPlaylistOk)
+        self.autoCompleteBox.itemDoubleClicked.connect(self.itemClicked)
+        self.piecesLineEdit.textChanged.connect(self.updateOptions)
 
     def newPlaylistOk(self):
         data = {"name":self.playlistNameLineEdit.text(), "pieces":[]}
@@ -16,3 +19,19 @@ class PlaylistDialog(QtGui.QDialog):
             fname = item.data(1)
             data["pieces"].append(fname)
         self.parent.addPlaylist(data)
+
+    def updateOptions(self):
+        text = self.piecesLineEdit.text()
+        results = self.parent.query(text)
+        self.autoCompleteBox.clear()
+        for result in results:
+            item = QtGui.QListWidgetItem(result[0])
+            item.setData(1, result[1])
+            self.autoCompleteBox.addItem(item)
+        self.autoCompleteFrame.show()
+        self.autoCompleteBox.show()
+
+    def itemClicked(self, current_item):
+        self.listWidget.addItem(current_item)
+        self.listWidget.show()
+        self.autoCompleteFrame.hide()
