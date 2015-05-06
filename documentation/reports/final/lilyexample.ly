8b347e7c929494deeb30f6e5cb25d95c13d13daf
Godley <charlotte.godley@hotmail.co.uk>
2015-05-06 22:19:01 +0100

    implemented most of david(s) comments
    - implemented most of what david whale said, up to the point of the
    appendix. work on issue #277. This includes adding a massive table
    which I’ve now moved to a separate file so a more accurate word count
    can be calculated: also deducted this from the word count script.
    - implemented all of david jones’ feedback, issue fix #276

---
+++ documentation/reports/final/lilyexample.ly
@@ -0,0 +1,8 @@
+
+
+\version "2.18.2" 
+\header {
+tagline=""
+}
+\relative c' {
+c4\pp c4\ppp }
--- documentation/reports/final/wordcount.sh
+++ documentation/reports/final/wordcount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VALUE="$(detex "Final Report.tex" | wc -w)"
 APPEND="$(detex appendices.tex | wc -w)"
-TOTAL=`expr $VALUE - $APPEND`
+SUMMARY="$(detex summarytable.tex | wc -w)"
+TOTAL=`expr $VALUE - $APPEND - $SUMMARY`
 echo "${TOTAL}";
