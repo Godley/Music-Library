b1e47ffc8e1e245f1b158e94c60d0d868de6b542
Godley <charlotte.godley@hotmail.co.uk>
2015-11-05 18:56:07 +0000

    Removed lilypond installation as it's not really necessary to run lib tests rn

--- implementation/primaries/exceptions.py
+++ implementation/primaries/exceptions.py
@@ -3,5 +3,5 @@ class LilypondNotInstalledException(BaseException):
     '''ERROR! LILYPOND NOT FOUND'''
 
 class APIKeyNotFoundException(BaseException):
-    """ ERROR: did not find API key environment variable. Please request one and set your environment variable
+    """ ERROR: Missing an API key.
     """
