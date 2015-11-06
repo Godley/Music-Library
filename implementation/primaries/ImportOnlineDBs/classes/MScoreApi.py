636a69b9ca56488d7e29b0852b83925a3baf1ba6
Godley <charlotte.godley@hotmail.co.uk>
2015-11-06 19:39:08 +0000

    gzip problem fix

--- implementation/primaries/ImportOnlineDBs/classes/MScoreApi.py
+++ implementation/primaries/ImportOnlineDBs/classes/MScoreApi.py
@@ -38,7 +38,8 @@ class MuseScoreApi(Api):
         :return: list of dictionaries containing metadata about all pieces licensed to modify commercially
         '''
         request = requests.get(self.endpoint, params=self.params)
-        raise(Exception(str(request.headers)))
+        if request.headers['content-encoding'] == 'gzip':
+            request.encoding = 'gzip'
         if request.encoding == 'gzip':
             buf = StringIO(request.content)
             f = gzip.GzipFile(fileobj=buf)
