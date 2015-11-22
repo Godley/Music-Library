a5ddab03938ca804711f0caec2f215de5b47664a
Charlotte Godley(home) <charlotte.godley@hotmail.co.uk>
2015-11-22 17:23:25 +0000

    work on issue #313
    - created unit tests for parsing themes
    - split themes and icons into two separate methods
    - made another method which finds and replaces slashes with back slashes if working in windows

--- implementation/primaries/GUI/helpers.py
+++ implementation/primaries/GUI/helpers.py
@@ -24,30 +24,64 @@ def get_base_dir(return_this_dir=False):
 def parseStyle(stylesheet, theme):
     results = []
     for line in stylesheet:
-        if "themes/" in line:
-            if sys.platform == 'win32':
-                line.replace('/', '\\')
-            else:
-                line.replace('\\', '/')
-            section1 = line
-            postfix = ""
-            while "themes" in section1:
-                portion = os.path.split(section1)
-                section1 = portion[0]
-                postfix = os.path.join(portion[1],postfix)
-            result_string = section1+os.path.join(get_base_dir(True), postfix)
-
-            if "icons" in result_string:
-                prefix = result_string
-                postfix = ""
-                while "icons" in prefix:
-                    split_path = result_string.split('icons')
-                    prefix = split_path[0]
-                    postfix = "".join([postfix, split_path[1]])
-                result_string = os.path.join(prefix, "icons", theme, postfix)
-            print(result_string)
-            results.append(result_string)
-
-        else:
-            results.append(line)
+        cleaned_line = cleanPath(line)
+        themed_path = parseThemePath(cleaned_line)
+        finished_path = parseIconPath(themed_path, theme)
+        results.append(finished_path)
     return "\n".join(results)
+
+def cleanPath(path):
+    path_to_parse = path
+    if sys.platform == 'win32':
+        path_to_parse = path.replace('/', '\\')
+    return path_to_parse
+
+def parseThemePath(path):
+    path_to_parse = path
+    prefix = ''
+    postfix = ''
+    parsed_path = path
+    if 'themes' in path_to_parse:
+        while 'themes' in path_to_parse:
+            split_path = os.path.split(path_to_parse)
+            if split_path[0] != 'themes':
+                prefix = split_path[0]
+            if postfix == '':
+                postfix = split_path[1]
+            elif split_path[1] != 'themes':
+                postfix = os.path.join(split_path[1], postfix)
+            path_to_parse = split_path[0]
+        if '(' in prefix:
+            parsed_path = prefix
+        else:
+            parsed_path = ''
+        if sys.platform == 'win32':
+            parsed_path += '\''
+        parsed_path = parsed_path + os.path.join(get_base_dir(True), 'themes')
+        if sys.platform == 'win32':
+            end_path = postfix.split(')')
+            if len(end_path) > 1:
+                parsed_path = os.path.join(parsed_path, end_path[0]) + "')" + end_path[1]
+            else:
+                parsed_path = os.path.join(parsed_path, postfix) + "'"
+        else:
+            parsed_path = os.path.join(parsed_path, postfix)
+    return parsed_path
+
+def parseIconPath(path, theme):
+    prefix = ''
+    postfix = ''
+    parsing_path = path
+    parsed_path = path
+    if 'icons' in parsing_path:
+        while 'icons' in parsing_path:
+            split_path = os.path.split(parsing_path)
+            if postfix == '' and split_path != 'icons':
+                postfix = split_path[1]
+            elif split_path[1] != 'icons':
+                postfix = os.path.join(split_path[1], postfix)
+            if 'icons' not in split_path[0]:
+                prefix = split_path[0]
+            parsing_path = split_path[0]
+        parsed_path = os.path.join(prefix, 'icons', theme, postfix)
+    return parsed_path
--- implementation/primaries/GUI/tests/testHelpers.py
+++ implementation/primaries/GUI/tests/testHelpers.py
@@ -5,5 +5,42 @@ class testStylesheet(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testWindowsStylesheetParse(self):
-        pass
+    def testCleanPath(self):
+        path = 'themes/item.png'
+        if sys.platform == 'win32':
+            expected = "themes\\item.png"
+        else:
+            expected = path
+        self.assertEqual(expected, helpers.cleanPath(path))
+
+    def testParseThemePath(self):
+        path = 'themes/item.png'
+        expected = "'" + os.path.join(helpers.get_base_dir(True), 'themes', 'item.png') + "'"
+        cleaned_path = helpers.cleanPath(path)
+        self.assertEqual(expected, helpers.parseThemePath(cleaned_path))
+
+    def testParseThemePathWithCSS(self):
+        line = 'background: url(/themes/zoom-out.png) center no-repeat;'
+        expected = "background: url('" + os.path.join(helpers.get_base_dir(True), 'themes', "zoom-out.png")+ '\') center no-repeat;'
+        cleaned_path = helpers.cleanPath(line)
+        self.assertEqual(expected, helpers.parseThemePath(cleaned_path))
+
+    def testParseIconPath(self):
+        path = 'themes/icons/item.png'
+        theme = 'ubuntu'
+        new_path = os.path.join(helpers.get_base_dir(True), 'themes', 'icons', 'ubuntu', 'item.png')
+        if sys.platform == 'win32':
+            expected = "'" + new_path + "'"
+        else:
+            expected = new_path
+        cleaned_path = helpers.cleanPath(path)
+        themed_path = helpers.parseThemePath(cleaned_path)
+        self.assertEqual(expected, helpers.parseIconPath(themed_path, theme))
+
+    def testParseIconPathWithCSS(self):
+        line = 'background: url(/themes/icons/zoom-out.png) center no-repeat;'
+        theme = 'ubuntu'
+        expected = os.path.join('background: url(', helpers.get_base_dir(True), 'themes', 'icons', theme, 'zoom-out.png) center no-repeat;')
+        cleaned_path = helpers.cleanPath(line)
+        themed_path = helpers.parseThemePath(cleaned_path)
+        self.assertEqual(expected, helpers.parseIconPath(themed_path, theme))
