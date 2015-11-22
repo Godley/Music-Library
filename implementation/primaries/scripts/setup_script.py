8a68f3addca0dbf8c9e8aae0e81c0377c39d4549
Charlotte Godley(home) <charlotte.godley@hotmail.co.uk>
2015-11-22 19:40:33 +0000

    updated version, further testing added
    - win_tests runs all tests but beforehand does a lilypond setup. Not really necessary in all cases but I wanted to thoroughly check the lilypond setup works properly

--- implementation/primaries/scripts/setup_script.py
+++ implementation/primaries/scripts/setup_script.py
@@ -2,29 +2,19 @@ import sys
 import os
 from implementation.primaries.exceptions import LilypondNotInstalledException
 from implementation.primaries.GUI.helpers import get_base_dir
-
+from MuseParse.classes.Output.helpers import setupLilypondClean as setupLilypond
 
 
 def do_setup(path=None):
-    defaults = ["/Applications/Lilypond.app/Contents/Resources/bin",
-                "C:/Program Files (x86)/LilyPond/usr/bin"]
-    filepath = None
-    lpond_path = None
-    if sys.platform == "darwin":
-        if path is None:
-            lpond_path = defaults[0]
-        else:
-            lpond_path = os.path.join(path, "Contents/Resources/bin")
-            filepath = os.path.join(lpond_path, "lilypond")
+    defaults = {'darwin': "/Applications/Lilypond.app/Contents/Resources/bin",
+                'win32': "C:/Program Files (x86)/LilyPond/usr/bin"}
+    n_path = path
+    if n_path is None:
+        n_path = defaults[sys.platform]
+    filename = {'darwin': 'lilypond.sh', 'win32': 'lilypond.exe'}
+    filepath = os.path.join(n_path, filename[sys.platform])
+    if os.path.exists(filepath):
+        setupLilypond(n_path)
+    else:
+        raise LilypondNotInstalledException('ERROR! Lilypond not installed or filepath incorrect. Please provide the folder which contains '+filename[sys.platform])
 
-    elif sys.platform == "win32" or sys.platform == "win64":
-        if path is None:
-            lpond_path = defaults[1]
-        else:
-            lpond_path = path
-            filepath = os.path.join(lpond_path, "lilypond.exe")
-    # if filepath is not None and os.path.exists(filepath):
-    #     setup_lilypond(lpond_path)
-    # else:
-    #     raise LilypondNotInstalledException(
-    #         'ERROR! Windows edition of Lilypond not in expected folder')
