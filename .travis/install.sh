b97c43a2848ed5dbe1abe14cca46e12487fd7c9a
Godley <charlotte.godley@hotmail.co.uk>
2016-08-18 17:57:11 +0100

    modified travis to include osx, added script to handle differences in install process

---
+++ .travis/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+    stty rows 40 columns 80 # set rows, columns in terminal
+    wget https://distfiles.macports.org/MacPorts/MacPorts-2.3.3.tar.bz2
+    tar xvfj MacPorts-2.3.3.tar.bz2
+    cd MacPorts-2.3.3
+    ./configure && sudo make install
+    export PATH=/opt/local/bin:$PATH
+    sudo port -v selfupdate
+    cp /opt/local/etc/macports/sources.conf .
+    echo "https://leanprover.github.io/macports/ports.tar" >> sources.conf
+    sudo mv -f sources.conf /opt/local/etc/macports/sources.conf
+
+    case "${TOXENV}" in
+        py33)
+            # Install some custom Python 3.3 requirements on OS X
+            sudo port install py33-poppler-pyqt4
+            ;;
+        py34)
+            sudo port install py34-poppler-pyqt4
+            ;;
+        py35)
+            sudo port install py35-poppler-pyqt4
+            ;;
+    esac
+else
+    # Install some custom requirements on Linux
+    sudo apt-get install python3-sip-dev
+fi
