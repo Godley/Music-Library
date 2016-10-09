50cf2800d8416e198106ba5a7a17016c72c23dcc
Godley <charlotte.godley@hotmail.co.uk>
2016-10-09 23:50:30 +0100

    fixed tempo parser tests

--- implementation/primaries/ExtractMetadata/tests/test_data_layer/test_tempo_parser.py
+++ implementation/primaries/ExtractMetadata/tests/test_data_layer/test_tempo_parser.py
@@ -102,13 +102,13 @@ class TestTempoParser(unittest.TestCase):
     def testParseOneWordTempo(self):
         entry = 'quaver=80'
         expected = {'beat': 'eighth', 'minute': 80,
-                    'beat_2': -1}
+                    'beat_2': None}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
 
     def testParseTwoWordTempo(self):
         entry = 'quaver=crotchet'
-        expected = {'beat': 'eighth', 'minute': -1,
+        expected = {'beat': 'eighth', 'minute': None,
                     'beat_2': 'quarter'}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
@@ -116,13 +116,13 @@ class TestTempoParser(unittest.TestCase):
     def testParseOneWordDottedTempo(self):
         entry = 'quaver.=80'
         expected = {'beat': 'eighth.', 'minute': 80,
-                    'beat_2': -1}
+                    'beat_2': None}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
 
     def testParseTwoWordDottedTempo(self):
         entry = 'quaver.=crotchet.'
-        expected = {'beat': 'eighth.', 'minute': -1,
+        expected = {'beat': 'eighth.', 'minute': None,
                     'beat_2': 'quarter.'}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
@@ -130,13 +130,13 @@ class TestTempoParser(unittest.TestCase):
     def testParseOneWordHalvedTempo(self):
         entry = 'semiquaver=80'
         expected = {'beat': '16th', 'minute': 80,
-                    'beat_2': -1}
+                    'beat_2': None}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
 
     def testParseHalvedDottedTempo(self):
         entry = 'semiquaver.=80'
         expected = {'beat': '16th.', 'minute': 80,
-                    'beat_2': -1}
+                    'beat_2': None}
         result = self.parser.decode(entry)
         self.assertDictEqual(expected, result)
