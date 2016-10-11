29aaa63029f9c265bb4e29eb994c62eb6cddd240
Godley <charlotte.godley@hotmail.co.uk>
2016-10-12 00:22:40 +0100

    removed code which was old + thus not being covered by tests

--- implementation/primaries/ExtractMetadata/classes/DataLayer/helpers.py
+++ implementation/primaries/ExtractMetadata/classes/DataLayer/helpers.py
@@ -1,27 +1,3 @@
-def extendJoinQuery(query_len, extender, operand, init_string=''):
-    query = ''
-    for i in range(query_len):
-        if i == 0:
-            query += init_string
-        query += extender
-        if i != query_len - 1:
-            query += operand
-    if query_len > 0:
-        query += ')'
-    return query
-
-
-def do_online_offline_query(query, piece_id_field, online=False):
-    new_query = query
-    if online:
-        new_query += ' AND EXISTS '
-    else:
-        new_query += ' AND NOT EXISTS '
-    new_query += '(SELECT * FROM sources WHERE piece_id = ' + \
-        piece_id_field + ')'
-    return new_query
-
-
 def get_if_exists(dict, key, default=0):
     if dict is not None:
         if key in dict:
